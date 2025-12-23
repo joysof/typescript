ea21c7b2d0c4b7c01b025329142c236de6658f4e
sajib hossen <sajibhossen560@gmail.com>
2025-12-23 14:00:20 +0600

    build-in data type

---
+++ data-type/index.js
@@ -0,0 +1,9 @@
+// built-in data type 
+// number , string , boolean , void , undefined , null 
+var userId;
+var userName;
+var isActivated;
+userId = 101;
+userName = "sajib Hossen";
+isActivated = true;
+console.log("userId :", userId);
---
+++ data-type/index.ts
@@ -0,0 +1,13 @@
+// built-in data type 
+// number , string , boolean , void , undefined , null 
+
+let  userId : number;
+
+let userName : string ;
+
+let isActivated : boolean;
+userId = 101;
+userName = "sajib Hossen";
+isActivated = true
+
+console.log("userId :", userId ,)
