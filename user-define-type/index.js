33c802065080fa4e1e85ffc75a404c48d04a0483
sajib hossen <sajibhossen560@gmail.com>
2025-12-23 17:52:02 +0600

    array data type

---
+++ user-define-type/index.js
@@ -0,0 +1,4 @@
+var userName;
+userName = ['sajib', 'hossen', 'joy'];
+console.log(userName[0]);
+console.log("i am ");
---
+++ user-define-type/index.ts
@@ -0,0 +1,6 @@
+let userName :string[]
+userName = ['sajib' , 'hossen' , 'joy']
+
+console.log(userName[0])
+
+console.log("i am ")
