df311b33dde9d9c4cd25076646856da24e412542
sajib hossen <sajibhossen560@gmail.com>
2025-12-23 17:57:31 +0600

    mixed data type

--- user-define-type/index.js
+++ user-define-type/index.js
@@ -1,4 +1,12 @@
+// array data type 
 var userName;
 userName = ['sajib', 'hossen', 'joy'];
 console.log(userName[0]);
 console.log("i am ");
+// tuple data type 
+// mixed data type 
+// work key and value 
+var user;
+user = [101, "sajib"];
+user.push(102, "joy");
+console.log(user);
--- user-define-type/index.ts
+++ user-define-type/index.ts
@@ -1,6 +1,21 @@
+// array data type 
+
 let userName :string[]
 userName = ['sajib' , 'hossen' , 'joy']
 
 console.log(userName[0])
 
 console.log("i am ")
+
+// tuple data type 
+
+// mixed data type 
+
+// work key and value 
+
+let user : [number , string];
+
+user = [101, "sajib"]
+
+user.push(102 , "joy")
+console.log(user)
