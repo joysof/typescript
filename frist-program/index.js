c82bf445a8148d622a5751b68837e67530a1c2df
sajib hossen <sajibhossen560@gmail.com>
2025-12-23 13:36:37 +0600

    using typescript with variable and function

--- frist-program/index.js
+++ frist-program/index.js
@@ -1,2 +1,17 @@
-var userName = "sajib Hossen";
+var userName;
+userName = "sajib Hossen";
 console.log(userName);
+var age = 25;
+console.log("age", age);
+// sympole function in javascript 
+// function addTowNumber (num1 , num2) {
+//     console.log(num1 + num2)
+// }
+// addTowNumber(20 , 30)
+// addTowNumber(20 , "30")
+// using typesctipt 
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(20, 30);
+addNumber(20, 30);
--- frist-program/index.ts
+++ frist-program/index.ts
@@ -1,2 +1,24 @@
-let userName = "sajib Hossen";
+
+let userName :string;
+userName  = "sajib Hossen"
 console.log(userName);
+
+let age = 25;
+console.log("age",age)
+
+// sympole function in javascript 
+
+// function addTowNumber (num1 , num2) {
+//     console.log(num1 + num2)
+// }
+
+// addTowNumber(20 , 30)
+// addTowNumber(20 , "30")
+
+// using typesctipt 
+
+function addNumber (num1 : number , num2: number){
+    console.log(num1 + num2)
+}
+addNumber(20 ,30)
+addNumber(20 ,30)
